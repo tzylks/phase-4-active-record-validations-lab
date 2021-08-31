0d0c4b8f218b63241483cbaf3c9fe6903230e139
TrevorZylks <trevorzylks@gmail.com>
2021-08-30 23:10:32 -0700

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :clickbait?
+
+    
+
+   
+
+    def clickbait?
+    
+    unless title && title.match(/(Won't Believe)|(Top \d)|(Secret)|(Guess)/)
+        errors.add(:title, "not clickbait")
+    end
+         
+    end
+    
+
+
 end
